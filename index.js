6a5afbd9d901c70090c83ec707f832ba0c8797f9
Md Kamrul Islam <kamruldca@gmail.com>
2023-11-08 22:16:30 +0600

    put the allJobs data easily

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pfigzq.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
