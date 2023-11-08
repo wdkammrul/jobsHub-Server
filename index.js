531b40fba498f41f8540153345a70fa7316af419
Md Kamrul Islam <kamruldca@gmail.com>
2023-11-08 18:12:33 +0600

    new start

--- index.js
+++ index.js
@@ -28,18 +28,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const jobCollection = client.db("jobsHub").collection("allJobs");
 
-    const jobCollection = client.db("jobsHub").collection('allJobs');
+    app.get("/allJobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-    app.get('/allJobs', async(req, res) => {
-         const cursor = jobCollection.find();
-         const result = await cursor.toArray()
-         res.send(result); 
-    })
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
