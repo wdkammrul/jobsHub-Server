1e2546490629219da9525e88fdc2ced96f92d13c
Md Kamrul Islam <kamruldca@gmail.com>
2023-11-09 13:08:55 +0600

    apply submit post

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
-app.use(express.json());
-
+app.use(cors()); app.use(express.json());
 console.log(process.env.DB_PASS);
 console.log(process.env.DB_USER);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pfigzq.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -26,9 +23,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
-    await client.connect();
+    // await client.connect();
 
     const jobCollection = client.db("jobsHub").collection("allJobs");
+    const applicationCollection = client.db("jobsHub").collection("apply");
 
     app.get("/allJobs", async (req, res) => {
       const cursor = jobCollection.find();
@@ -36,7 +34,34 @@ async function run() {
       res.send(result);
     });
 
+
+    app.post("/allJobs", async (req, res) => {
+      const job = req.body;
+      console.log(job)
+      const result = await jobCollection.insertOne(job);
+      res.send(result);
+    });
+
+
+    app.get("/apply", async (req, res) => {
+      const cursor = applicationCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    app.post("/apply", async (req, res) => {
+      const apply = req.body;
+      console.log(apply)
+      const result = await applicationCollection.insertOne(apply);
+      res.send(result);
+    });
+
+
+    
    
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
